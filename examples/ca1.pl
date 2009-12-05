f3f68d5d827a9e11be0be11084823df2f7451671
dbrunton <dbrunton@gmail.com>
2009-12-05 00:26:12 -0500

    nighty night

--- examples/ca1.pl
+++ examples/ca1.pl
@@ -3,10 +3,9 @@ BEGIN { @*INC.push('../lib') };
 use Automata::Cellular;
 use Automata::Cellular::Rule;
 
-my Automata::Cellular $ca .= new(:rule_number(30));
+my Automata::Cellular $ca .= new(:rule_number(30), :display_width(67), :steps(30));
 say "Rule Number " ~ $ca.rule.rule_number();
 say $ca.rule.pretty();
-say $ca.prettystate();
-while $ca.next() {
+repeat {
   say $ca.prettystate();
-}
+} while $ca.next();
